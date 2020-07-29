e04ccc5d2c958c7a88b7b76fd1e222e4fe6aed07
j0sephmerheb <info@josephmerheb.net>
2020-07-29 14:22:42 +0200

    3 edit / delete / show pages

--- routes/web/tws.js
+++ routes/web/tws.js
@@ -12,7 +12,7 @@ var list = (req, res, msg = '') => {
         .lean()
         .exec()
         .then(tws => {
-            res.render('tws', {
+            res.render('tws/list', {
                 tws: tws,
                 error: error,
                 msg: msg,
@@ -44,6 +44,36 @@ Router.get('/', (req, res) =>{
     list(req, res)
 });
 
+Router.get('/show/:twId', (req, res) => {
+    Tw.findOne({_id: req.params.twId})
+        .lean()
+        .exec()
+        .then(tw => {
+            res.render('tws/show', {
+                tw: tw
+            });
+        })
+        .catch(err => {
+            error = err;
+            console.error(error);
+        });
+});
+
+Router.get('/edit/:twId', (req, res) => {
+    Tw.findOne({_id: req.params.twId})
+        .lean()
+        .exec()
+        .then(tw => {
+            res.render('tws/edit', {
+                tw: tw
+            });
+        })
+        .catch(err => {
+            error = err;
+            console.error(error);
+        });
+});
+
 Router.get('/:type/:msg', (req, res) => {
     var msg = {
         type: req.params.type,
@@ -68,6 +98,30 @@ Router.post('/', (req, res) => {
             })
     } else {
         // res.status(500).json({error: "Please put some values"});
+        res.redirect('/tws/danger/Please put some value');
+    }
+})
+
+Router.post('/update/:twId', (req, res) => {
+    twId = req.params.twId;
+    message = req.body.message;
+
+    if (req.body.message && req.body.message != "") {
+        Tw.update({ _id: twId }, {
+                $set: {
+                    message: req.body.message,
+                }
+            })
+            .exec()
+            .then(tw => {
+                res.redirect('/tws/success/Tw well updated');
+            })
+            .catch(err => {
+                // res.status(500).json({ error: err });
+                res.redirect('/tws/danger/'+err);
+            });
+    } else {
+        // res.status(500).json({ error: 'Merci de remplire tous les champs' });
         res.redirect('/tws/danger/Please put some value');
     }
 })
